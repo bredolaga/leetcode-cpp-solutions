62042fd982f5b3dfc9b082fb2cd6d6d949835d71
bredolaga <62011884+bredolaga@users.noreply.github.com>
2025-10-26 10:33:46 +0500

    Create 27. Remove Element.cpp

---
+++ C++/Easy/27. Remove Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int e = 0;
+       for (int i = 0;i < nums.size();i++){
+        if (nums[i] == val){
+            nums.erase(nums.begin() + i);
+            i--;
+        }
+        else e++;
+       }
+       return e; 
+    }
+};
